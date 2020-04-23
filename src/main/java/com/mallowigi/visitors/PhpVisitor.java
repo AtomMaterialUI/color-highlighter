f8ad7f104762be726e0c88be48644c16d326588b
helio <heliosaian@gmail.com>
2020-04-23 20:32:27 +0300

    add php

--- src/main/java/com/mallowigi/visitors/PhpVisitor.java
+++ src/main/java/com/mallowigi/visitors/PhpVisitor.java
@@ -3,26 +3,26 @@ package com.mallowigi.visitors;
 import com.intellij.codeInsight.daemon.impl.HighlightVisitor;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiUtilCore;
+import com.jetbrains.php.lang.psi.PhpFile;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.mallowigi.search.ColorSearchEngine;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.psi.KtFile;
 
 import java.awt.*;
 
-public class KotlinVisitor extends ColorVisitor {
+public class PhpVisitor extends ColorVisitor {
   @Override
   public boolean suitableForFile(@NotNull final PsiFile file) {
-    return file instanceof KtFile;
+    return file instanceof PhpFile;
   }
 
   @Override
   public void visit(@NotNull final PsiElement element) {
-    if (!"INTEGER_CONSTANT".equals(PsiUtilCore.getElementType(element).toString())) { // NON-NLS
+    if (!(element instanceof StringLiteralExpression)) {
       return;
     }
 
-    final String value = element.getText();
+    final String value = ((StringLiteralExpression) element).getContents();
     final Color color = ColorSearchEngine.getColor(value);
 
     if (color != null) {
@@ -33,6 +33,6 @@ public class KotlinVisitor extends ColorVisitor {
   @NotNull
   @Override
   public HighlightVisitor clone() {
-    return new KotlinVisitor();
+    return new PhpVisitor();
   }
 }
