912b6d53e53e45a8cfd62410c1ebb63fe573982e
helio <heliosaian@gmail.com>
2020-04-23 10:54:42 +0300

    start working on kotlin (added sql)

--- src/main/java/com/mallowigi/visitors/KotlinVisitor.java
+++ src/main/java/com/mallowigi/visitors/KotlinVisitor.java
@@ -3,22 +3,22 @@ package com.mallowigi.visitors;
 import com.intellij.codeInsight.daemon.impl.HighlightVisitor;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.util.PsiUtilCore;
 import com.mallowigi.search.ColorSearchEngine;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.ruby.ruby.lang.psi.RFile;
 
 import java.awt.*;
 
-public class RubyVisitor extends ColorVisitor {
+public class KotlinVisitor extends ColorVisitor {
   @Override
   public boolean suitableForFile(@NotNull final PsiFile file) {
-    return file instanceof RFile;
+    return file instanceof PsiJavaFile;
   }
 
   @Override
   public void visit(@NotNull final PsiElement element) {
-    if (!"string content".equals(PsiUtilCore.getElementType(element).toString())) { //NON-NLS
+    if (!"INTEGER_LITERAL".equals(PsiUtilCore.getElementType(element).toString())) { // NON-NLS
       return;
     }
 
@@ -33,6 +33,6 @@ public class RubyVisitor extends ColorVisitor {
   @NotNull
   @Override
   public HighlightVisitor clone() {
-    return new RubyVisitor();
+    return new KotlinVisitor();
   }
 }
