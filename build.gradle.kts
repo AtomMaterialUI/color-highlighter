a6a5415a479ae18c529b6d94d2a826800c43ed41
Elior <heliosaian@gmail.com>
2021-06-23 14:16:20 +0300

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,19 +24,21 @@
  *
  */
 
+
+
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
   // Java support
   id("java")
   // Kotlin support
-  id("org.jetbrains.kotlin.jvm") version "1.5.0-M2"
+  id("org.jetbrains.kotlin.jvm") version "1.5.10"
   // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
   id("org.jetbrains.intellij") version "1.0"
   // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
   id("org.jetbrains.changelog") version "1.1.2"
   // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-  id("io.gitlab.arturbosch.detekt") version "1.16.0"
+  id("io.gitlab.arturbosch.detekt") version "1.17.1"
   // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
   id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
@@ -61,11 +63,11 @@ repositories {
 }
 
 dependencies {
-  detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
+  detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
   implementation("com.thoughtworks.xstream:xstream:1.4.16")
   implementation("org.javassist:javassist:3.27.0-GA")
   implementation("com.mixpanel:mixpanel-java:1.5.0")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.0-M2")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.10")
 }
 
 // Configure gradle-intellij-plugin plugin.
