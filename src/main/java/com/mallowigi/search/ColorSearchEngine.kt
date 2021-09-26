22af09e0d876dcb2ba0ff6f0ce55405f2dcad8aa
Elior <heliosaian@gmail.com>
2021-09-26 10:46:19 +0300

    chore(colorSearchEngine): convert to kotlin

--- src/main/java/com/mallowigi/search/ColorSearchEngine.kt
+++ src/main/java/com/mallowigi/search/ColorSearchEngine.kt
@@ -1,109 +1,105 @@
-package com.mallowigi.search;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-2021 Elior "Mallowigi" Boukhobza
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
 
-import com.google.common.collect.Lists;
-import com.mallowigi.search.parsers.ColorParserFactory;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+package com.mallowigi.search
 
-import java.awt.*;
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Pattern;
+import com.mallowigi.search.parsers.ColorParserFactory
+import java.awt.Color
+import java.util.regex.Pattern
 
-public enum ColorSearchEngine {
-  ;
-
+object ColorSearchEngine {
   // region COLOR_PATTERNS
-  private static final List<Pattern> COLOR_PATTERNS = Collections.unmodifiableList(
-    Lists.newArrayList(
-      Pattern.compile(
-        "((#\\p{XDigit}{6}\\b)|(#\\p{XDigit}{3}\\b))"
-      ),
-      // #123456 or #333
-      Pattern.compile(
-        "\\b((rgb\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*\\))|(rgb\\s*\\(\\s*\\p{Digit}{1," +
-          "3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*\\)))"
-      ),
-      // rgb(128, 128, 128)
-      Pattern.compile(
-        "\\b((rgba\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*,\\s*[0-9.]{1,3}\\s*\\))|" +
-          "(rgba\\s*\\" +
-          "(\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*[0-9.]{1,3}\\s*\\)))"
-      ),
-      // rgba(128, 128, 128, 0)
-      Pattern.compile(
-        "\\b(hsl\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*\\))"
-      ),
-      // hsl(0, 12, 120)
-      Pattern.compile(
-        "\\b(hsla\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*[0-9.]{1,3}\\s*\\))"
-      ),
-      // hsla(0, 12, 120, 1)
-      Pattern.compile(
-        "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
-      ),
-      // Color(0x123456)
-      Pattern.compile(
-        "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(true|false)\\s*\\))"
-      ),
-      // Color(0x123456, true)
-      Pattern.compile(
-        "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
-      ),
-      // Color(128, 0, 12)
-      Pattern.compile(
-        "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])" +
-          "?[0-9a-fA-F]+\\s*\\))"
-      ),
-      // Color(123, 0, 12, 12)
-      Pattern.compile(
-        "\\b(Color\\s*\\(\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*\\))"
-      ),
-      // Color(12f, 32f, 9f)
-      Pattern.compile(
-        "\\b(Color\\s*\\(\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*\\))"
-      ),
-      // Color(12f, 32f, 9f, 0f)
-      Pattern.compile(
-        "\\b(Color\\.[a-zA-Z_]+)\\b"
-      ),
-      // Color.yellow
-      Pattern.compile(
-        "\\b(ColorUIResource\\s*\\(\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*\\))"
-      ),
-      // ColorUIResource(12f, 12f, 12f)
-      Pattern.compile(
-        "\\b(ColorUIResource\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
-      ),
-      // ColorUIResource(0x123456)
-      Pattern.compile(
-        "\\b(ColorUIResource\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
-      ),
-      // ColorUIResource(12f, 12f, 12f)
-      Pattern.compile(
-        "\\b(ColorUIResource\\.[a-zA-Z_]+)\\b"
-      ),
-      // ColorUIResource.black
-      Pattern.compile(
-        "\\b([a-zA-Z]+)\\b"
-      )
-      // aliceblue
-    ));
+  private val COLOR_PATTERNS = listOf(
+    Pattern.compile(
+      "((#\\p{XDigit}{6}\\b)|(#\\p{XDigit}{3}\\b))"
+    ),  // #123456 or #333
+    Pattern.compile(
+      "\\b((rgb\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*\\))|(rgb\\s*\\(\\s*\\p{Digit}{1," +
+        "3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*\\)))"
+    ),  // rgb(128, 128, 128)
+    Pattern.compile(
+      "\\b((rgba\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}\\s*,\\s*[0-9.]{1,3}\\s*\\))|" +
+        "(rgba\\s*\\" +
+        "(\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*[0-9.]{1,3}\\s*\\)))"
+    ),  // rgba(128, 128, 128, 0)
+    Pattern.compile(
+      "\\b(hsl\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*\\))"
+    ),  // hsl(0, 12, 120)
+    Pattern.compile(
+      "\\b(hsla\\s*\\(\\s*\\p{Digit}{1,3}\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*\\p{Digit}{1,3}%\\s*,\\s*[0-9.]{1,3}\\s*\\))"
+    ),  // hsla(0, 12, 120, 1)
+    Pattern.compile(
+      "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
+    ),  // Color(0x123456)
+    Pattern.compile(
+      "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(true|false)\\s*\\))"
+    ),  // Color(0x123456, true)
+    Pattern.compile(
+      "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
+    ),  // Color(128, 0, 12)
+    Pattern.compile(
+      "\\b(Color\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])" +
+        "?[0-9a-fA-F]+\\s*\\))"
+    ),  // Color(123, 0, 12, 12)
+    Pattern.compile(
+      "\\b(Color\\s*\\(\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*\\))"
+    ),  // Color(12f, 32f, 9f)
+    Pattern.compile(
+      "\\b(Color\\s*\\(\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*\\))"
+    ),  // Color(12f, 32f, 9f, 0f)
+    Pattern.compile(
+      "\\b(Color\\.[a-zA-Z_]+)\\b"
+    ),  // Color.yellow
+    Pattern.compile(
+      "\\b(ColorUIResource\\s*\\(\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*,\\s*[0-9.]+f?\\s*\\))"
+    ),  // ColorUIResource(12f, 12f, 12f)
+    Pattern.compile(
+      "\\b(ColorUIResource\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
+    ),  // ColorUIResource(0x123456)
+    Pattern.compile(
+      "\\b(ColorUIResource\\s*\\(\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*,\\s*(0[xX])?[0-9a-fA-F]+\\s*\\))"
+    ),  // ColorUIResource(12f, 12f, 12f)
+    Pattern.compile(
+      "\\b(ColorUIResource\\.[a-zA-Z_]+)\\b"
+    ),  // ColorUIResource.black
+    Pattern.compile(
+      "\\b([a-zA-Z]+)\\b"
+    ) // aliceblue
+  )
   // endregion
-
   /**
    * Try to parse a color using the provided formats
-   * TODO use Factory
    */
-  @SuppressWarnings("DynamicRegexReplaceableByCompiledPattern")
-  @Nullable
-  public static Color getColor(@NotNull final String text) {
-    try {
-      final String normalizedText = text.replaceAll("\"", "").replaceAll("'", "");
-      return ColorParserFactory.getParser(normalizedText).parseColor(normalizedText);
-    } catch (final RuntimeException e) {
-      return null;
+  fun getColor(text: String): Color? {
+    return try {
+      val normalizedText = text.replace("\"".toRegex(), "").replace("'".toRegex(), "")
+      ColorParserFactory.getParser(normalizedText).parseColor(normalizedText)
+    } catch (e: RuntimeException) {
+      null
     }
   }
-
 }
+
--- src/main/java/com/mallowigi/utils/ColorInfo.kt
+++ src/main/java/com/mallowigi/utils/ColorInfo.kt
@@ -1,172 +1,99 @@
-package com.mallowigi.utils;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-2021 Elior "Mallowigi" Boukhobza
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
 
-import com.mallowigi.colors.ColorsService;
+@file:Suppress("MemberVisibilityCanBePrivate")
 
-import java.awt.*;
+package com.mallowigi.utils
 
-@SuppressWarnings("ClassWithTooManyFields")
-public final class ColorInfo {
-  private static boolean hexUppercase;
-  private final String txtRGB;
-  private final String txtHSB;
-  private final String txtHSL;
-  private final String shortHex;
-  private final String hex;
-  private final String decimalRGB;
-  private final String percentRGB;
-  private final String decimalRGBa;
-  private final String percentRGBa;
-  private final String hsl;
-  private final String hsla;
-  private final String colorFFF;
-  private final String colorFFFF;
-  private final String colorI;
-  private final String colorIB;
-  private final String colorIII;
-  private final String colorIIII;
-  private final String colorUIResourceFFF;
-  private final String colorUIResourceI;
-  private final String colorUIResourceIII;
-  private final Color color;
+import com.mallowigi.colors.ColorsService.Companion.instance
+import java.awt.Color
+import kotlin.math.roundToInt
 
-  @SuppressWarnings("HardCodedStringLiteral")
-  public ColorInfo(final Color color) {
-    this.color = color;
-    final int argb = color.getRGB();
-    final int rgb = argb & 0xffffff;
-    final float[] comps = color.getRGBComponents(null);
-    final float[] hslComps = ColorUtils.RGBtoHSL(color.getRed(), color.getGreen(), color.getBlue(), null);
-    final float[] hsbComps = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
+data class ColorInfo(val color: Color) {
+  val txtRGB: String
+  val txtHSB: String
+  val txtHSL: String
+  val shortHex: String
+  val hex: String
+  val decimalRGB: String
+  val percentRGB: String
+  val decimalRGBa: String
+  val percentRGBa: String
+  val hsl: String
+  val hsla: String
+  val colorFFF: String
+  val colorFFFF: String
+  val colorI: String
+  val colorIB: String
+  val colorIII: String
+  val colorIIII: String
+  val colorUIResourceFFF: String
+  val colorUIResourceI: String
+  val colorUIResourceIII: String
 
-    txtRGB = String.format("%d, %d, %d", color.getRed(), color.getGreen(), color.getBlue());
-    txtHSB = String.format("%d, %d%%, %d%%", toDegrees(hsbComps[0]), toPercent(hsbComps[1]), toPercent(hsbComps[2]));
-    txtHSL = String.format("%d, %d%%, %d%%", toDegrees(hslComps[0]), toPercent(hslComps[1]), toPercent(hslComps[2]));
-
-    shortHex = String.format(hexUppercase ? "#%1X%1X%1X" : "#%1x%1x%1x", color.getRed() / 16, color.getGreen() / 16,
-      color.getBlue() / 16);
-    hex = String.format(hexUppercase ? "#%06X" : "#%06x", rgb);
-    decimalRGB = String.format("rgb(%d, %d, %d)", color.getRed(), color.getGreen(), color.getBlue());
-    percentRGB = String.format("rgb(%d%%, %d%%, %d%%)", toPercent(comps[0]), toPercent(comps[1]), toPercent(comps[2]));
-    decimalRGBa = String.format("rgba(%d, %d, %d, %.2f)", color.getRed(), color.getGreen(), color.getBlue(), comps[3]);
-    percentRGBa = String.format("rgba(%d%%, %d%%, %d%%, %.2f)", toPercent(comps[0]), toPercent(comps[1]), toPercent(comps[2]), comps[3]);
-    hsl = String.format("hsl(%d, %d%%, %d%%)", toDegrees(hslComps[0]), toPercent(hslComps[1]), toPercent(hslComps[2]));
-    hsla = String.format("hsla(%d, %d%%, %d%%, %.2f)", toDegrees(hslComps[0]), toPercent(hslComps[1]), toPercent(hslComps[2]), comps[3]);
-    colorFFF = String.format("Color(%.2ff, %.2ff, %.2ff)", comps[0], comps[1], comps[2]);
-    colorFFFF = String.format("Color(%.2ff, %.2ff, %.2ff, %.2ff)", comps[0], comps[1], comps[2], comps[3]);
-    colorI = String.format(hexUppercase ? "Color(0x%06X)" : "Color(0x%06x)", rgb);
-    colorIB = String.format(hexUppercase ? "Color(0x%08X, true)" : "Color(0x%08x, true)", argb);
-    colorIII = String.format("Color(%d, %d, %d)", color.getRed(), color.getGreen(), color.getBlue());
-    colorIIII = String.format("Color(%d, %d, %d, %d)", color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
-    colorUIResourceFFF = String.format("ColorUIResource(%.2ff, %.2ff, %.2ff)", comps[0], comps[1], comps[2]);
-    colorUIResourceI = String.format(hexUppercase ? "ColorUIResource(0x%06X)" : "ColorUIResource(0x%06x)", rgb);
-    colorUIResourceIII = String.format("ColorUIResource(%d, %d, %d)", color.getRed(), color.getGreen(), color.getBlue());
+  init {
+    val argb = color.rgb
+    val rgb = argb and 0xffffff
+    val comps = color.getRGBComponents(null)
+    val hslComps = ColorUtils.RGBtoHSL(color.red, color.green, color.blue, null)
+    val hsbComps = Color.RGBtoHSB(color.red, color.green, color.blue, null)
+    txtRGB = String.format("%d, %d, %d", color.red, color.green, color.blue)
+    txtHSB = String.format("%d, %d%%, %d%%", toDegrees(hsbComps[0]), toPercent(hsbComps[1]), toPercent(hsbComps[2]))
+    txtHSL = String.format("%d, %d%%, %d%%", toDegrees(hslComps[0]), toPercent(hslComps[1]), toPercent(hslComps[2]))
+    shortHex = String.format(if (isHexUppercase) "#%1X%1X%1X" else "#%1x%1x%1x", color.red / 16, color.green / 16,
+      color.blue / 16)
+    hex = String.format(if (isHexUppercase) "#%06X" else "#%06x", rgb)
+    decimalRGB = String.format("rgb(%d, %d, %d)", color.red, color.green, color.blue)
+    percentRGB = String.format("rgb(%d%%, %d%%, %d%%)", toPercent(comps[0]), toPercent(comps[1]), toPercent(comps[2]))
+    decimalRGBa = String.format("rgba(%d, %d, %d, %.2f)", color.red, color.green, color.blue, comps[3])
+    percentRGBa = String.format("rgba(%d%%, %d%%, %d%%, %.2f)", toPercent(comps[0]), toPercent(comps[1]), toPercent(comps[2]), comps[3])
+    hsl = String.format("hsl(%d, %d%%, %d%%)", toDegrees(hslComps[0]), toPercent(hslComps[1]), toPercent(hslComps[2]))
+    hsla = String.format("hsla(%d, %d%%, %d%%, %.2f)", toDegrees(hslComps[0]), toPercent(hslComps[1]), toPercent(hslComps[2]), comps[3])
+    colorFFF = String.format("Color(%.2ff, %.2ff, %.2ff)", comps[0], comps[1], comps[2])
+    colorFFFF = String.format("Color(%.2ff, %.2ff, %.2ff, %.2ff)", comps[0], comps[1], comps[2], comps[3])
+    colorI = String.format(if (isHexUppercase) "Color(0x%06X)" else "Color(0x%06x)", rgb)
+    colorIB = String.format(if (isHexUppercase) "Color(0x%08X, true)" else "Color(0x%08x, true)", argb)
+    colorIII = String.format("Color(%d, %d, %d)", color.red, color.green, color.blue)
+    colorIIII = String.format("Color(%d, %d, %d, %d)", color.red, color.green, color.blue, color.alpha)
+    colorUIResourceFFF = String.format("ColorUIResource(%.2ff, %.2ff, %.2ff)", comps[0], comps[1], comps[2])
+    colorUIResourceI = String.format(if (isHexUppercase) "ColorUIResource(0x%06X)" else "ColorUIResource(0x%06x)", rgb)
+    colorUIResourceIII = String.format("ColorUIResource(%d, %d, %d)", color.red, color.green, color.blue)
   }
 
-  public static boolean isHexUppercase() {
-    return hexUppercase;
-  }
+  val sVGName: String?
+    get() = instance.findSVGName(color)
 
-  public static void setHexUppercase(final boolean hexUppercase) {
-    ColorInfo.hexUppercase = hexUppercase;
-  }
+  val javaName: String?
+    get() = instance.findJavaName(color)
 
-  private static int toPercent(final float val) {
-    return Math.round(val * 100);
-  }
+  companion object {
+    var isHexUppercase: Boolean = false
 
-  private static int toDegrees(final float val) {
-    return Math.round(val * 359);
-  }
+    private fun toPercent(`val`: Float): Int = (`val` * 100).roundToInt()
 
-  public String getTxtRGB() {
-    return txtRGB;
-  }
-
-  public String getTxtHSB() {
-    return txtHSB;
-  }
-
-  public String getTxtHSL() {
-    return txtHSL;
-  }
-
-  public String getShortHex() {
-    return shortHex;
-  }
-
-  public String getHex() {
-    return hex;
-  }
-
-  public String getSVGName() {
-    return ColorsService.getInstance().findSVGName(color);
-  }
-
-  public String getJavaName() {
-    return ColorsService.getInstance().findJavaName(color);
-  }
-
-  public Color getColor() {
-    return color;
-  }
-
-  public String getDecimalRGB() {
-    return decimalRGB;
-  }
-
-  public String getPercentRGB() {
-    return percentRGB;
-  }
-
-  public String getDecimalRGBa() {
-    return decimalRGBa;
-  }
-
-  public String getPercentRGBa() {
-    return percentRGBa;
-  }
-
-  public String getHsl() {
-    return hsl;
-  }
-
-  public String getHsla() {
-    return hsla;
-  }
-
-  public String getColorFFF() {
-    return colorFFF;
-  }
-
-  public String getColorFFFF() {
-    return colorFFFF;
-  }
-
-  public String getColorI() {
-    return colorI;
-  }
-
-  public String getColorIB() {
-    return colorIB;
-  }
-
-  public String getColorIII() {
-    return colorIII;
-  }
-
-  public String getColorIIII() {
-    return colorIIII;
-  }
-
-  public String getColorUIResourceFFF() {
-    return colorUIResourceFFF;
-  }
-
-  public String getColorUIResourceI() {
-    return colorUIResourceI;
-  }
-
-  public String getColorUIResourceIII() {
-    return colorUIResourceIII;
+    private fun toDegrees(`val`: Float): Int = (`val` * 359).roundToInt()
   }
 }
