993436a6df197d3d4b5ab3e37c52a4b4cc7ee670
Elior <heliosaian@gmail.com>
2023-08-13 09:21:18 +0300

    feat(lua): support for toml

---
+++ src/test/java/colors.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { writable } from 'svelte/store';
+
+  const isValid = writable(true); // you can customize the initial value.
+</script>
+
+<style>
+  label[aria-invalid="true"] {
+    color: red;
+  }
+
+  label[aria-invalid="false"] {
+    color: black;
+  }
+</style>
+
+<label aria-invalid={$isValid}></label>
